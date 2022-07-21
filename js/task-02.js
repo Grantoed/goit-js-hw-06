cc92afcb7468ae6eae3d1c60dfd2cfaf8b85713b
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2022-07-21 18:17:31 +0300

    task 2 solved

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
+const ingredientsRef = document.querySelector("#ingredients");
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const elements = ingredients.map((ingredient) => {
+  const liRef = document.createElement("li");
+  liRef.textContent = ingredient;
+  liRef.classList.add("item");
+  return liRef;
+});
+
+ingredientsRef.append(...elements);
