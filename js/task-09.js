a386c3d15e93ffd7230ae72dd1538cd373818ed8
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2022-07-23 22:48:06 +0300

    Task 9 solved

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const bodyRef = document.querySelector("body");
+const buttonRef = document.querySelector(".change-color");
+const textRef = document.querySelector(".color");
+
+buttonRef.addEventListener("click", () => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  textRef.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
